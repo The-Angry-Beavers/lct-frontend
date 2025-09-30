4fae3631b76b1f814125fae165400e9f4196375c
Valery Kovalev <kovalev.vxx@gmail.com>
2025-09-30 21:56:35 +0300

    feat: start page bg

--- src/pages/start-page/index.tsx
+++ src/pages/start-page/index.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router";
 import { useMusicPlayer } from "@/packages/music-player";
+import bgImage from "@/shared/assets/background.webp?url";
 import MuteButton from "@/shared/components/mute-button";
 import PageContainer from "@/shared/ui/page-container";
 
@@ -7,10 +8,15 @@ const StartPage = () => {
 	const navigate = useNavigate();
 	const { play } = useMusicPlayer();
 	return (
-		<PageContainer>
-			<div className="flex-1 grid place-items-center">
+		<PageContainer className="relative">
+			<img
+				src={bgImage}
+				alt=""
+				className="w-full h-full object-cover absolute object-center"
+			/>
+			<div className="flex-1 grid place-items-center relative">
 				<div className="flex flex-col gap-4 items-center">
-					<h1 className="font-halvar">НАЗВАНИЕ ИГРЫ</h1>
+					<h1 className="font-halvar">Отдел по мечтам</h1>
 					<button
 						onClick={() => {
 							setTimeout(() => {
