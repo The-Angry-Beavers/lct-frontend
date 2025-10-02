63250bf0c59f96e77a8be8067f1716f5e6607a6a
Valery Kovalev <kovalev.vxx@gmail.com>
2025-10-02 23:32:35 +0300

    fix

--- src/pages/game-page/components/game-field/index.tsx
+++ src/pages/game-page/components/game-field/index.tsx
@@ -139,7 +139,7 @@ const ClientMessage = () => {
 				>
 					<div className="bg-white translate-y-[100%] rounded-[1rem] border-2 border-black">
 						<div className="text-black px-4 text-sm py-2">
-							<Typewriter text={client.message} speed={35} />
+							<Typewriter text={client.message} speed={5} />
 						</div>
 						<div className="absolute left-[2rem] top-[-1.25rem] text-sm rounded-full px-2 py-[0.0625rem] border-2 border-white bg-[#1919EF]">
 							{client.first_name}
