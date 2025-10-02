4d0564c54d78e5d922078855d964bd34d84b8375
Valery Kovalev <kovalev.vxx@gmail.com>
2025-10-02 15:30:44 +0300

    game html

---
+++ src/pages/prizes-page/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import PageContainer from "@/shared/ui/page-container";
+
+const PrizesPage = () => {
+	return <PageContainer></PageContainer>;
+};
+
+export default PrizesPage;
